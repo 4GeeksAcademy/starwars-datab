6aeee6ca778d14c46a3213ef55fc36f81ecee7e3
Moyunlimited <moy.estime@gmail.com>
2025-04-30 17:27:50 +0000

    fin

--- src/models.py
+++ src/models.py
@@ -1,19 +1,65 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    login: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "login": self.login
             # do not serialize the password, its a security breach
+        }
+
+
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"))
+    
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id
+        }
+
+    class Characters(db.Model):
+        id: Mapped[int] = mapped_column(primary_key=True)
+        name: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+        gender: Mapped[str] = mapped_column(String(60), nullable=False)
+        height: Mapped[str] = mapped_column(String(60), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height
+        }
+
+    class Planets(db.Model):
+        id: Mapped[int] = mapped_column(primary_key=True)
+        name: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+        climate: Mapped[str] = mapped_column(String(60), nullable=False)
+        gravity: Mapped[str] = mapped_column(String(60), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "gravity": self.gravity
         }
